546942f2624f28fbcf7dd5c166722c6a3c699b26
Kodyvim <softdevcoder@gmail.com>
2022-08-13 15:44:24 -0400

    Initial commit after development

---
+++ js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What country has the highest life expectancy",
+    answer: "Hong Kong",
+    options: [
+      "China",
+      "USA",
+      "Hong Kong",
+      "Singapore"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What sports car company manufactures the 911",
+    answer: "Porsche",
+    options: [
+      "Porsche",
+      "Ferrari",
+      "Lamborghini",
+      "Tesla"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the max length of a TikTok video?",
+    answer: "60 Seconds",
+    options: [
+      "20 Seconds",
+      "40 Seconds",
+      "60 Seconds",
+      "75 Seconds"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What country drinks the most coffee per capita?",
+    answer: "Finland",
+    options: [
+      "Finland",
+      "Franch",
+      "Spain",
+      "USA"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is a group of pandas known as?",
+    answer: "An embarrassment",
+    options: [
+      "Unity",
+      "An embarrassment",
+      "Couple",
+      "Zimn"
+    ]
+  },
+   {
+     numb: 6,
+    question: "In what country would you find Mount Kilimanjaro?",
+    answer: "Tanzania",
+   options: [
+     "Mali",
+     "Tanzania",
+     "Morocco",
+     "Nigeria"
+  ]
+  },
+  {
+     numb: 7,
+    question: "Kratos is the main character of what video game series?",
+    answer: "God of War",
+   options: [
+     "PUBG",
+     "Among Us",
+     "God of War",
+     "Call of Duty"
+  ]
+  },
+  {
+     numb: 8,
+    question: "What software company is headquartered in Redmond, Washington",
+    answer: "Microsoft",
+   options: [
+     "Google",
+     "StackUpHQ",
+     "Apple",
+     "Microsoft"
+  ]
+  },
+  {
+     numb: 9,
+    question: "What is the world's fastest bird",
+    answer: "The Peregrine Falcon",
+   options: [
+     "Vulture",
+     "The Peregrine Falcon",
+     "Blue Jay",
+     "geese"
+  ]
+  },
+  {
+     numb: 10,
+    question: "How many bones do we have in an ear? ",
+    answer: "3",
+   options: [
+     "2",
+     "6",
+     "3",
+     "4"
+  ]
+  },
+  /**{
+     numb: 11,
+    question: "Your Question is Here",
+    answer: "Correct answer of the question is here",
+   options: [
+     "Option 1",
+     "option 2",
+     "option 3",
+     "option 4"
+  ]
+  }, sample here**/ 
+];
+
+// you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
